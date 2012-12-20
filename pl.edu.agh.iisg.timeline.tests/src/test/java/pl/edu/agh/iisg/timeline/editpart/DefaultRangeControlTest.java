313a72b13203c08c7beb0bfff845cbd2c14ad7f3
Rafal Leszko <leszko@.agh.edu.pl>
2012-12-20 15:46:29 +0100

    Add DefaultRangeControl

---
+++ pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/editpart/DefaultRangeControlTest.java
@@ -0,0 +1,86 @@
+package pl.edu.agh.iisg.timeline.editpart;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DefaultRangeControlTest {
+
+    private final static int HEIGHT = 100;
+
+    private final static int INERTIA = 50;
+
+    private final static int MARGIN = 500;
+
+    private DefaultRangeControl range = new DefaultRangeControl(HEIGHT, INERTIA, MARGIN);
+
+    @Test
+    public void testInitRange() {
+        // when
+        range.setScrollPosition(0);
+
+        // then
+        assertTrue(range.isChanged());
+        assertEquals(MARGIN + HEIGHT, range.getEnd());
+        assertEquals(0, range.getStart());
+    }
+
+    @Test
+    public void changeRangeNotOutsideInertia() {
+        // given
+        range.setScrollPosition(0);
+
+        // when
+        range.setScrollPosition(55);
+
+        // then
+        assertFalse(range.isChanged());
+        assertEquals(MARGIN + HEIGHT, range.getEnd());
+        assertEquals(0, range.getStart());
+    }
+
+    @Test
+    public void changeOutsideInertiaInsideLeftMargin() {
+        // given
+        range.setScrollPosition(0);
+
+        // when
+        range.setScrollPosition(300);
+
+        // then
+        assertTrue(range.isChanged());
+        assertEquals(300 + HEIGHT + MARGIN, range.getEnd());
+        assertEquals(0, range.getStart());
+    }
+
+    @Test
+    public void changeOutsideInertiaOutsideLeftMargin() {
+        // given
+        range.setScrollPosition(0);
+
+        // when
+        range.setScrollPosition(1000);
+
+        // then
+        assertTrue(range.isChanged());
+        assertEquals(1000 + HEIGHT + MARGIN, range.getEnd());
+        assertEquals(1000 - MARGIN, range.getStart());
+    }
+
+    @Test
+    public void changeDoubleTheSame() {
+        // given
+        range.setScrollPosition(0);
+
+        // given
+        range.setScrollPosition(1000);
+        range.setScrollPosition(1000);
+
+        // then
+        assertFalse(range.isChanged());
+        assertEquals(1000 + HEIGHT + MARGIN, range.getEnd());
+        assertEquals(1000 - MARGIN, range.getStart());
+    }
+}
--- pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/editpart/IRangeControl.java
+++ pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/editpart/IRangeControl.java
@@ -1,13 +1,20 @@
 package pl.edu.agh.iisg.timeline.editpart;
 
+/**
+ * A class designed to control and determine how much of the model space should be visible. When the scroll position if changed the range of
+ * visible elements should change.
+ *
+ * @author leszko
+ *
+ */
 public interface IRangeControl {
 
-	void setScrollPosition(int position);
+    void setScrollPosition(int position);
 
-	boolean isChanged();
+    boolean isChanged();
 
-	int getStart();
+    int getStart();
 
-	int getEnd();
+    int getEnd();
 
 }

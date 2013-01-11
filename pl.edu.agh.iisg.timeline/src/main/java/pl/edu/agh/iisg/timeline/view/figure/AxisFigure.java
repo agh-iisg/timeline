eec7fb106f281d79cada7177652ff6482b62131d
Rafal Leszko <leszko@.agh.edu.pl>
2013-01-11 17:19:27 +0100

    Apply first visual changes.

--- pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/view/figure/AxisFigure.java
+++ pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/view/figure/AxisFigure.java
@@ -1,10 +1,6 @@
 package pl.edu.agh.iisg.timeline.view.figure;
 
 import org.eclipse.draw2d.BorderLayout;
-import org.eclipse.draw2d.ColorConstants;
-import org.eclipse.draw2d.Figure;
-import org.eclipse.draw2d.GridData;
-import org.eclipse.draw2d.GridLayout;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.RectangleFigure;
 
@@ -21,31 +17,15 @@ public class AxisFigure extends RectangleFigure {
 
     private void init() {
         setBackgroundColor(FigureConstants.AXIS_BACKGROUND);
+        setForegroundColor(FigureConstants.AXIS_FOREGROUND);
         setLayoutManager(new BorderLayout());
         setPreferredSize(FigureConstants.AXIS_WIDTH, FigureConstants.AXIS_HEIGHT);
     }
 
     private void addLabel(String name) {
         Label label = new Label(name);
+        label.setFont(FigureConstants.AXIS_FONT);
         add(label);
-        setConstraint(label, BorderLayout.TOP);
+        setConstraint(label, BorderLayout.LEFT);
     }
-
-    private void addLine() {
-        Figure figure = new Figure();
-        GridLayout layout = new GridLayout(1, false);
-        layout.marginWidth = WIDTH / 2;
-        figure.setLayoutManager(layout);
-        add(figure);
-        setConstraint(figure, BorderLayout.CENTER);
-
-        RectangleFigure rect = new RectangleFigure();
-        rect.setForegroundColor(ColorConstants.green);
-        rect.setBackgroundColor(ColorConstants.green);
-        rect.setSize(3, 10);
-
-        figure.add(rect);
-        figure.setConstraint(rect, new GridData(GridData.FILL_VERTICAL));
-    }
-
 }
--- pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/view/figure/FigureConstants.java
+++ pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/view/figure/FigureConstants.java
@@ -1,18 +1,26 @@
 package pl.edu.agh.iisg.timeline.view.figure;
 
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Display;
 
 public final class FigureConstants {
 
-    public static final int AXIS_WIDTH = 1340;
+    public static final int AXIS_WIDTH = 335;
 
-    public static final int AXIS_HEIGHT = 300;
+    public static final int AXIS_HEIGHT = 80;
 
     public static final int AXIS_GAP = 30;
 
     public static final int AXIS_BOTTOM_MARGIN = 30;
 
     public static final Color AXIS_BACKGROUND = new Color(null, 194, 193, 193);
+
+    public static final Color AXIS_FOREGROUND = ColorConstants.black;
+
+    public static final Font AXIS_FONT = new Font(Display.getDefault(), "Franklin Gothic Demi", 13, SWT.NONE);
 
     public static final int HEADER_HEIGHT = AXIS_HEIGHT + AXIS_BOTTOM_MARGIN;
 

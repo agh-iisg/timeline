dbf0d4b3cabb14a9ed825b23529c9f29f8300b1b
Rafal Leszko <rafal.leszko@gmail.com>
2013-01-21 19:51:43 +0100

    Change tests to use Mockito (instead of EasyMock).

--- pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/editpart/dynamic/DynamicModelRefresherTest.java
+++ pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/editpart/dynamic/DynamicModelRefresherTest.java
@@ -2,164 +2,153 @@ package pl.edu.agh.iisg.timeline.editpart.dynamic;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
-import pl.edu.agh.iisg.timeline.editpart.dynamic.DynamicModelRefresher;
-import pl.edu.agh.iisg.timeline.editpart.dynamic.IRangeControl;
-import pl.edu.agh.iisg.timeline.editpart.dynamic.ModelRefresh;
 import pl.edu.agh.iisg.timeline.model.AxisElement;
 import pl.edu.agh.iisg.timeline.model.Separator;
 import pl.edu.agh.iisg.timeline.positioner.IPositioner;
 
 public class DynamicModelRefresherTest {
 
-	private DynamicModelRefresher refresher;
+    private DynamicModelRefresher refresher;
 
-	private IPositioner positioner;
+    private IPositioner positioner;
 
-	private IRangeControl range;
+    private IRangeControl range;
 
-	private AxisElement element1 = EasyMock.createMock(AxisElement.class);
-	private AxisElement element2 = EasyMock.createMock(AxisElement.class);
-	private AxisElement element3 = EasyMock.createMock(AxisElement.class);
-	private AxisElement element4 = EasyMock.createMock(AxisElement.class);
+    private AxisElement element1 = mock(AxisElement.class);
 
-	private Separator separator1 = EasyMock.createMock(Separator.class);
-	private Separator separator2 = EasyMock.createMock(Separator.class);
-	private Separator separator3 = EasyMock.createMock(Separator.class);
-	private Separator separator4 = EasyMock.createMock(Separator.class);
+    private AxisElement element2 = mock(AxisElement.class);
 
-	private Collection<AxisElement> elements1 = Arrays.asList(element1,
-			element2, element3);
+    private AxisElement element3 = mock(AxisElement.class);
 
-	private Collection<Separator> separators1 = Arrays.asList(separator1,
-			separator2, separator3);
+    private AxisElement element4 = mock(AxisElement.class);
 
-	private Collection<AxisElement> elements2 = Arrays.asList(element2,
-			element3, element4);
-	private Collection<Separator> separators2 = Arrays.asList(separator1,
-			separator4);
-	private Collection<AxisElement> elements2minus1 = Arrays.asList(element4);
-	private Collection<Separator> separators2minus1 = Arrays.asList(separator4);
-	private Collection<AxisElement> elements1minus2 = Arrays.asList(element1);
-	private Collection<Separator> separators1minus2 = Arrays.asList(separator2,
-			separator3);
+    private Separator separator1 = mock(Separator.class);
 
-	@Before
-	public void setUp() {
-		positioner = EasyMock.createMock(IPositioner.class);
-		range = EasyMock.createMock(IRangeControl.class);
-		refresher = new DynamicModelRefresher(positioner, range);
-	}
+    private Separator separator2 = mock(Separator.class);
 
-	@Test
-	public void testShouldNotRefresh() {
-		// given
-		range.setScrollPosition(EasyMock.anyInt());
-		EasyMock.expect(range.isChanged()).andReturn(false);
-		EasyMock.replay(range);
+    private Separator separator3 = mock(Separator.class);
 
-		// when
-		ModelRefresh refresh = refresher.refresh(0);
+    private Separator separator4 = mock(Separator.class);
 
-		// then
-		assertFalse(refresh.shouldRefresh());
-		assertTrue(refresh.getElementsToAdd().isEmpty());
-		assertTrue(refresh.getElementsToRemove().isEmpty());
-		assertTrue(refresh.getSeparatorsToAdd().isEmpty());
-		assertTrue(refresh.getSeparatorsToRemove().isEmpty());
-	}
+    private Collection<AxisElement> elements1 = Arrays.asList(element1, element2, element3);
 
-	@Test
-	public void testInitRefresh() {
-		// given
-		prepareMocksForInitRefresh();
+    private Collection<Separator> separators1 = Arrays.asList(separator1, separator2, separator3);
 
-		// when
-		ModelRefresh refresh = refresher.refresh(0);
+    private Collection<AxisElement> elements2 = Arrays.asList(element2, element3, element4);
 
-		// then
-		assertTrue(refresh.shouldRefresh());
-		assertTheSameElements(elements1, refresh.getElementsToAdd());
-		assertTrue(refresh.getElementsToRemove().isEmpty());
-		assertTheSameElements(separators1, refresh.getSeparatorsToAdd());
-		assertTrue(refresh.getSeparatorsToRemove().isEmpty());
-	}
+    private Collection<Separator> separators2 = Arrays.asList(separator1, separator4);
 
-	private void assertTheSameElements(Collection<?> elements1,
-			Collection<?> elements2) {
-		assertTrue(elements1.containsAll(elements2));
-		assertTrue(elements2.containsAll(elements1));
+    private Collection<AxisElement> elements2minus1 = Arrays.asList(element4);
 
-	}
+    private Collection<Separator> separators2minus1 = Arrays.asList(separator4);
 
-	private void prepareMocksForInitRefresh() {
-		mockRangeFirstRefresh();
-		EasyMock.replay(range);
+    private Collection<AxisElement> elements1minus2 = Arrays.asList(element1);
 
-		mockPositionerFirstRefresh();
-		EasyMock.replay(positioner);
-	}
+    private Collection<Separator> separators1minus2 = Arrays.asList(separator2, separator3);
 
-	@Test
-	public void testChangeRangeRefresh() {
-		// given
-		prepareMocksForSecondRefresh();
+    @Before
+    public void setUp() {
+        positioner = mock(IPositioner.class);
+        range = mock(IRangeControl.class);
+        refresher = new DynamicModelRefresher(positioner, range);
+    }
 
-		// when
-		refresher.refresh(0);
-		ModelRefresh refresh = refresher.refresh(150);
+    @Test
+    public void testShouldNotRefresh() {
+        // given
+        when(range.isChanged()).thenReturn(false);
 
-		// then
-		assertTrue(refresh.shouldRefresh());
-		assertTheSameElements(elements2minus1, refresh.getElementsToAdd());
-		assertTheSameElements(elements1minus2, refresh.getElementsToRemove());
-		assertTheSameElements(separators2minus1, refresh.getSeparatorsToAdd());
-		assertTheSameElements(separators1minus2,
-				refresh.getSeparatorsToRemove());
-	}
+        // when
+        ModelRefresh refresh = refresher.refresh(0);
 
-	private void prepareMocksForSecondRefresh() {
-		mockRangeFirstRefresh();
-		mockRangeSecondRefresh();
-		EasyMock.replay(range);
+        // then
+        assertFalse(refresh.shouldRefresh());
+        assertTrue(refresh.getElementsToAdd().isEmpty());
+        assertTrue(refresh.getElementsToRemove().isEmpty());
+        assertTrue(refresh.getSeparatorsToAdd().isEmpty());
+        assertTrue(refresh.getSeparatorsToRemove().isEmpty());
+    }
 
-		mockPositionerFirstRefresh();
-		mockPositionerSecondRefresh();
-		EasyMock.replay(positioner);
-	}
+    @Test
+    public void testInitRefresh() {
+        // given
+        prepareMocksForInitRefresh();
 
-	private void mockRangeFirstRefresh() {
-		range.setScrollPosition(0);
-		EasyMock.expect(range.isChanged()).andReturn(true);
-		EasyMock.expect(range.getStart()).andReturn(0);
-		EasyMock.expect(range.getEnd()).andReturn(100);
-	}
+        // when
+        ModelRefresh refresh = refresher.refresh(0);
 
-	private void mockRangeSecondRefresh() {
-		range.setScrollPosition(150);
-		EasyMock.expect(range.isChanged()).andReturn(true);
-		EasyMock.expect(range.getStart()).andReturn(50);
-		EasyMock.expect(range.getEnd()).andReturn(250);
-	}
+        // then
+        assertTrue(refresh.shouldRefresh());
+        assertTheSameElements(elements1, refresh.getElementsToAdd());
+        assertTrue(refresh.getElementsToRemove().isEmpty());
+        assertTheSameElements(separators1, refresh.getSeparatorsToAdd());
+        assertTrue(refresh.getSeparatorsToRemove().isEmpty());
+    }
 
-	private void mockPositionerFirstRefresh() {
-		EasyMock.expect(positioner.getElementsByPosition(0, 100)).andReturn(
-				elements1);
-		EasyMock.expect(positioner.getSeparatorsByPosition(0, 100)).andReturn(
-				separators1);
-	}
+    private void assertTheSameElements(Collection<?> elements1, Collection<?> elements2) {
+        assertTrue(elements1.containsAll(elements2));
+        assertTrue(elements2.containsAll(elements1));
 
-	private void mockPositionerSecondRefresh() {
-		EasyMock.expect(positioner.getElementsByPosition(50, 250)).andReturn(
-				elements2);
-		EasyMock.expect(positioner.getSeparatorsByPosition(50, 250)).andReturn(
-				separators2);
-	}
+    }
+
+    private void prepareMocksForInitRefresh() {
+        mockRangeFirstRefresh();
+
+        mockPositionerFirstRefresh();
+    }
+
+    @Test
+    public void testChangeRangeRefresh() {
+        // given
+        prepareMocksForTwoRefreshes();
+
+        // when
+        refresher.refresh(0);
+        ModelRefresh refresh = refresher.refresh(150);
+
+        // then
+        assertTrue(refresh.shouldRefresh());
+        assertTheSameElements(elements2minus1, refresh.getElementsToAdd());
+        assertTheSameElements(elements1minus2, refresh.getElementsToRemove());
+        assertTheSameElements(separators2minus1, refresh.getSeparatorsToAdd());
+        assertTheSameElements(separators1minus2, refresh.getSeparatorsToRemove());
+    }
+
+    private void prepareMocksForTwoRefreshes() {
+        mockRangeForTwoRefreshes();
+
+        mockPositionerFirstRefresh();
+        mockPositionerSecondRefresh();
+    }
+
+    private void mockRangeFirstRefresh() {
+        when(range.isChanged()).thenReturn(true);
+        when(range.getStart()).thenReturn(0);
+        when(range.getEnd()).thenReturn(100);
+    }
+
+    private void mockRangeForTwoRefreshes() {
+        when(range.isChanged()).thenReturn(true, true);
+        when(range.getStart()).thenReturn(0, 50);
+        when(range.getEnd()).thenReturn(100, 250);
+    }
+
+    private void mockPositionerFirstRefresh() {
+        when(positioner.getElementsByPosition(0, 100)).thenReturn(elements1);
+        when(positioner.getSeparatorsByPosition(0, 100)).thenReturn(separators1);
+    }
+
+    private void mockPositionerSecondRefresh() {
+        when(positioner.getElementsByPosition(50, 250)).thenReturn(elements2);
+        when(positioner.getSeparatorsByPosition(50, 250)).thenReturn(separators2);
+    }
 }

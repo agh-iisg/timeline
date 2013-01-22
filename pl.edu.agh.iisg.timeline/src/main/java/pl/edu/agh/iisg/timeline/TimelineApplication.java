9d4a933e26c98fccd5502c15e3a565d2e3583f73
Michał Idzik <scorpmi@gmail.com>
2013-01-22 17:26:04 +0100

    [CAST-1752] Fonts are loaded from ttf files

--- pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/TimelineApplication.java
+++ pl.edu.agh.iisg.timeline/src/main/java/pl/edu/agh/iisg/timeline/TimelineApplication.java
@@ -5,15 +5,19 @@ import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
+import pl.edu.agh.iisg.timeline.util.FontsLoader;
+
 public class TimelineApplication implements IApplication {
 
 	private static Display display;
+	private static final String FONTS_DIR = "fonts";
 
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		display = PlatformUI.createDisplay();
 
 		try {
+			FontsLoader.loadFromDirectory(FONTS_DIR);
 			int returnCode = PlatformUI.createAndRunWorkbench(display,
 					new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART) {

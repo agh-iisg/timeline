93af70ac1f46b5de1cf40ad243aab2654c8784aa
Rafal Leszko <rafal.leszko@gmail.com>
2013-01-22 13:06:48 +0100

    Externalize polish strings.
    In the product the encoding was utf-8, and in the development cp1250,
    now it's externalized, so it's independent.

---
+++ pl.edu.agh.iisg.timeline.integration.tests/src/test/java/pl/edu/agh/iisg/timeline/util/Messages.java
@@ -0,0 +1,66 @@
+package pl.edu.agh.iisg.timeline.util;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+    private static final String BUNDLE_NAME = "pl.edu.agh.iisg.timeline.util.messages"; //$NON-NLS-1$
+
+    public static String DataGenerator__SampleAxisName20;
+
+    public static String DataGenerator__SampleAxisName21;
+
+    public static String DataGenerator_SampleAxisName1;
+
+    public static String DataGenerator_SampleAxisName2;
+
+    public static String DataGenerator_SampleElement1;
+
+    public static String DataGenerator_SampleElement10;
+
+    public static String DataGenerator_SampleElement11;
+
+    public static String DataGenerator_SampleElement12;
+
+    public static String DataGenerator_SampleElement13;
+
+    public static String DataGenerator_SampleElement14;
+
+    public static String DataGenerator_SampleElement15;
+
+    public static String DataGenerator_SampleElement16;
+
+    public static String DataGenerator_SampleElement17;
+
+    public static String DataGenerator_SampleElement18;
+
+    public static String DataGenerator_SampleElement19;
+
+    public static String DataGenerator_SampleElement2;
+
+    public static String DataGenerator_SampleElement20;
+
+    public static String DataGenerator_SampleElement21;
+
+    public static String DataGenerator_SampleElement22;
+
+    public static String DataGenerator_SampleElement3;
+
+    public static String DataGenerator_SampleElement4;
+
+    public static String DataGenerator_SampleElement5;
+
+    public static String DataGenerator_SampleElement6;
+
+    public static String DataGenerator_SampleElement7;
+
+    public static String DataGenerator_SampleElement8;
+
+    public static String DataGenerator_SampleElement9;
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+    }
+}

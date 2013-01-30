4672f73776692028f800d7ae59be2ef8a843daf2
Rafal Leszko <leszko@.agh.edu.pl>
2013-01-30 16:55:13 +0100

    Correct one test.
    Now, all tests should pass.

--- pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/model/CreatingTimelineDiagramDemo.java
+++ pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/model/CreatingTimelineDiagramDemo.java
@@ -2,51 +2,51 @@ package pl.edu.agh.iisg.timeline.model;
 
 import java.util.Random;
 
-import org.junit.Test;
-
 import com.google.common.base.Stopwatch;
 
 public class CreatingTimelineDiagramDemo {
 
-	private static final int AXES_COUNT = 10;
-	private static final long AXIS_ELEMENTS_COUNT = 10L;
+    private static final int AXES_COUNT = 10;
 
-	private static int axesCounter = 0;
-	private static long elementsCounter = 0;
+    private static final long AXIS_ELEMENTS_COUNT = 10L;
 
-	private Random rand = new Random();
+    private static int axesCounter = 0;
 
-	@Test
-	public void createDiagramPerformanceTest() {
-		Stopwatch watch = new Stopwatch();
-		watch.start();
-		TimelineDiagram diagram = TimelineDiagram.builder().minDateTime(1L)
-				.maxDateTime(2L).build();
+    private static long elementsCounter = 0;
 
-		for (int i = 0; i < AXES_COUNT; i++) {
-			Axis axis = createSampleAxis();
-			diagram.addAxis(axis);
-			for (long j = 0; j < AXIS_ELEMENTS_COUNT; j++) {
-				diagram.addAxisElement(createSampleAxisElement(axis));
-			}
-		}
+    private Random rand = new Random();
 
-		watch.stop();
-		System.out.println(String.format("Elapsed time %s", watch.toString()));
+    public static void main(String[] args) {
+        new CreatingTimelineDiagramDemo().createDiagramPerformanceTest();
+    }
 
-	}
+    public void createDiagramPerformanceTest() {
+        Stopwatch watch = new Stopwatch();
+        watch.start();
+        TimelineDiagram diagram = TimelineDiagram.builder().minDateTime(1L).name("name").maxDateTime(2L).build();
 
-	private Axis createSampleAxis() {
-		Axis axis = Axis.builder().name(String.format("axis%d", axesCounter++))
-				.build();
-		return axis;
-	}
+        for (int i = 0; i < AXES_COUNT; i++) {
+            Axis axis = createSampleAxis();
+            diagram.addAxis(axis);
+            for (long j = 0; j < AXIS_ELEMENTS_COUNT; j++) {
+                diagram.addAxisElement(createSampleAxisElement(axis));
+            }
+        }
 
-	private AxisElement createSampleAxisElement(Axis owner) {
-		AxisElement element = AxisElement.builder().owner(owner)
-				.name(String.format("element %d", elementsCounter++))
-				.date(rand.nextLong()).build();
-		return element;
-	}
+        watch.stop();
+        System.out.println(String.format("Elapsed time %s", watch.toString()));
+
+    }
+
+    private Axis createSampleAxis() {
+        Axis axis = Axis.builder().name(String.format("axis%d", axesCounter++)).build();
+        return axis;
+    }
+
+    private AxisElement createSampleAxisElement(Axis owner) {
+        AxisElement element = AxisElement.builder().owner(owner).name(String.format("element %d", elementsCounter++)).date(rand.nextLong())
+                .build();
+        return element;
+    }
 
 }

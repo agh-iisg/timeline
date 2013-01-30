22905982be479259c8c0f757cf3627c8acd40a95
Rafal Leszko <leszko@.agh.edu.pl>
2013-01-30 16:40:46 +0100

    Change demo project name.
    "integration.tests" => "example"

--- pl.edu.agh.iisg.timeline.integration.tests/src/test/java/pl/edu/agh/iisg/timeline/integration/Activator.java
+++ pl.edu.agh.iisg.timeline.integration.tests/src/test/java/pl/edu/agh/iisg/timeline/integration/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "pl.edu.agh.iisg.timeline.integration.tests"; //$NON-NLS-1$
+    public static final String PLUGIN_ID = "pl.edu.agh.iisg.timeline.example"; //$NON-NLS-1$
 
     // The shared instance
     private static Activator plugin;

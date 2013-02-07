b057f4cc209fe737eddeb446daaaef66b22110a3
Rafal Leszko <leszko@.agh.edu.pl>
2013-02-07 17:23:53 +0100

    Check manual serialization.

--- pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/model/SaveToFilePerformanceTest.java
+++ pl.edu.agh.iisg.timeline.tests/src/test/java/pl/edu/agh/iisg/timeline/model/SaveToFilePerformanceTest.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.text.ParseException;
+import java.util.Collection;
+import java.util.List;
 
 import pl.edu.agh.iisg.timeline.integration.DataGenerator;
 
@@ -29,18 +31,70 @@ public class SaveToFilePerformanceTest {
     }
 
     private static void test() throws ParseException, IOException {
-        TimelineDiagram diagram = DataGenerator.createRealDataDiagram(10, 10, false);
+        TimelineDiagram diagram = DataGenerator.createRealDataDiagram(10, 100000, false);
 
         storeToTempFileAndPrintStatistics(diagram);
+        storeTextToTempFileAndPrintStatistics(diagram);
     }
 
-    private static void storeToTempFileAndPrintStatistics(TimelineDiagram diagram) throws IOException {
-        File temp = File.createTempFile("tempfile", ".tmp");
+    private static void storeTextToTempFileAndPrintStatistics(TimelineDiagram diagram) throws IOException {
+        System.out.println("2. Manual text serialization:");
+        File temp = File.createTempFile("tempfile2", ".tmp");
         long start = System.currentTimeMillis();
-        store(temp, diagram);
+
+        storeTextDiagram(temp, diagram);
+
         long end = System.currentTimeMillis();
         System.out.println("time: " + (end - start));
         System.out.println("size: " + temp.length());
+        System.out.println();
+    }
+
+    private static void storeTextDiagram(File file, TimelineDiagram diagram) {
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            writeElements(oos, diagram.getElements());
+            writeAxes(oos, diagram.getAxes());
+
+            oos.close();
+            fos.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private static void writeAxes(ObjectOutputStream oos, List<Axis> axes) throws IOException {
+        for (Axis axis : axes) {
+            oos.writeChars(axis.getName());
+            oos.writeObject(axis.getImageDesc());
+        }
+
+    }
+
+    private static void writeElements(ObjectOutputStream oos, Collection<Element> elements) throws IOException {
+        for (Element element : elements) {
+            oos.writeChars(element.getAxis().getName());
+            oos.writeChars(element.getTitle());
+            oos.writeChars(element.getDescription());
+            oos.writeLong(element.getDate());
+        }
+
+    }
+
+    private static void storeToTempFileAndPrintStatistics(TimelineDiagram diagram) throws IOException {
+        System.out.println("1. Java Serialization:");
+        File temp = File.createTempFile("tempfile", ".tmp");
+        long start = System.currentTimeMillis();
+
+        store(temp, diagram);
+
+        long end = System.currentTimeMillis();
+        System.out.println("time: " + (end - start));
+        System.out.println("size: " + temp.length());
+        System.out.println();
 
     }
 
